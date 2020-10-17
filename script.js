92f4275eca680214a076b42b57130356d8a41699
kristinmoon <71245424+kristinmoon@users.noreply.github.com>
2020-10-16 21:44:51 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var questions = [
+  { q: "Commonly used data types DO NOT include:", 1: "Strings", 2: "Booleans", 3: "Alerts", 4: "Numbers" },
+  { q: "The condition in an if/else statement is enclosed within ____.", 1: "Quotes", 2: "Curly brackets", 3: "Parentheses", 4: "Square brackets" },
+  { q: "Arrays in JavaScript can be used to store ____.", 1: "Numbers and strings", 2: "Other arrays", 3: "Booleans", 4: "All of the above" },
+  { q: "String values must be enclosed within ____ when being assigned to variables.", 1: "Commas", 2: "Curly brackets", 3: "Quotes", 4: "Parentheses" },
+  { q: "A very useful tool used during development and debugging for printing content to the debugger is:", 1: "JavaScript", 2: "Terminal / Bash", 3: "For loops", 4: "Console.log" }
+]
