bd6b1e5e35ebb9c9ff34e16ddcbc70067520f09a
kristinmoon <71245424+kristinmoon@users.noreply.github.com>
2020-10-16 23:13:29 -0700

    added CSS

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
 var questions = [
-  { q: "Commonly used data types DO NOT include:", 1: "Strings", 2: "Booleans", 3: "Alerts", 4: "Numbers" },
-  { q: "The condition in an if/else statement is enclosed within ____.", 1: "Quotes", 2: "Curly brackets", 3: "Parentheses", 4: "Square brackets" },
-  { q: "Arrays in JavaScript can be used to store ____.", 1: "Numbers and strings", 2: "Other arrays", 3: "Booleans", 4: "All of the above" },
-  { q: "String values must be enclosed within ____ when being assigned to variables.", 1: "Commas", 2: "Curly brackets", 3: "Quotes", 4: "Parentheses" },
-  { q: "A very useful tool used during development and debugging for printing content to the debugger is:", 1: "JavaScript", 2: "Terminal / Bash", 3: "For loops", 4: "Console.log" }
-]
+  { q: "Commonly used data types DO NOT include:", ans: ["Strings", "Booleans", "Alerts", "Numbers"] },
+  { q: "The condition in an if/else statement is enclosed within ____.", ans: ["Quotes", "Curly brackets", "Parentheses", "Square brackets"] },
+  { q: "Arrays in JavaScript can be used to store ____.", ans: ["Numbers and strings", "Other arrays", "Booleans", "All of the above"] },
+  { q: "String values must be enclosed within ____ when being assigned to variables.", ans: ["Commas", "Curly brackets", "Quotes", "Parentheses"] },
+  { q: "A very useful tool used during development and debugging for printing content to the debugger is:", ans: ["JavaScript", "Terminal / Bash", "For loops", "Console.log"] }
+];
+
+document.getElementById("btnStart").addEventListener("click", displayQuestion);
+
+function displayQuestion() {
+  var question = questions.pop();
+  if (question) {
+    var answer = confirm(question.q);
+    if (answer === question.ans) {
+      alert("Correct");
+    }
+    else {
+      alert("Wrong");
+    }
+  }
+};
+
+
+
+
+
