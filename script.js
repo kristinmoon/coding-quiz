35454d4b69e982be152684ecccae4aede71de6e8
kristinmoon <71245424+kristinmoon@users.noreply.github.com>
2020-10-17 15:50:10 -0700

    questions cycle through with buttons for the choices

--- script.js
+++ script.js
@@ -1,9 +1,57 @@
 var timerEl = document.getElementById("countdown");
-var startBtn = document.getElementById("start");
+var start = document.getElementById("start");
+var quiz = document.getElementById("quiz");
+var question = document.getElementById("question");
+var choiceA = document.getElementById("A");
+var choiceB = document.getElementById("B");
+var choiceC = document.getElementById("C");
+var choiceD = document.getElementById("D");
+var scoreDiv = document.getElementById("score");
+var timeLeft = 75;
 
-
-
-
+// questions
+var questions = [
+  {
+    question: "The condition in an if/else statement is enclosed within ____.",
+    choiceA: "Quotes",
+    choiceB: "Curly brackets",
+    choiceC: "Parentheses",
+    choiceD: "Square brackets",
+    correct: "B"
+  },
+  {
+    question: "Commonly used data types DO NOT include:",
+    choiceA: "Strings",
+    choiceB: "Booleans",
+    choiceC: "Alerts",
+    choiceD: "Numbers",
+    correct: "C"
+  },
+  {
+    question: "Arrays in JavaScript can be used to store ____.",
+    choiceA: "Numbers and strings",
+    choiceB: "Other arrays",
+    choiceC: "Booleans",
+    choiceD: "All of the above",
+    correct: "D"
+  },
+  {
+    question: "String values must be enclosed within ____ when being assigned to variables.",
+    choiceA: "Commas",
+    choiceB: "Curly brackets",
+    choiceC: "Quotes",
+    choiceD: "Parentheses",
+    correct: "C"
+  },
+  {
+    question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+    choiceA: "JavaScript",
+    choiceB: "Terminal / Bash",
+    choiceC: "For loops",
+    choiceD: "Console.log",
+    correct: "D"
+  }
+];
 
 
 
@@ -25,76 +73,60 @@ function countdown() {
   }, 1000);
 }
 
+function startQuiz() {
+  start.style.display = "none";
+  countdown()
+  renderQuestion();
+  quiz.style.display = "block";
+}
+
+function scoreRender() {
+  score.style.display = "block";
+  // show form page to enter initials and score 
+  score.textContent = "Your final score is " + timeLeft.value + "."
+}
 
 
-// questions
-var myQuestions = [
-  {
-    question: "The condition in an if/else statement is enclosed within ____.",
-    answers: {
-      a: "Quotes",
-      b: "Curly brackets",
-      c: "Parentheses",
-      d: "Square brackets"
-    },
-    correctAnswer: "b"
-  },
-  {
-    question: "Commonly used data types DO NOT include:",
-    answers: {
-      a: "Strings",
-      b: "Booleans",
-      c: "Alerts",
-      d: "Numbers"
-    },
-    correctAnswer: "c"
-  },
-  {
-    question: "Arrays in JavaScript can be used to store ____.",
-    answers: {
-      a: "Numbers and strings",
-      b: "Other arrays",
-      c: "Booleans",
-      d: "All of the above"
-    },
-    correctAnswer: "d"
-  },
-  {
-    question: "String values must be enclosed within ____ when being assigned to variables.",
-    answers: {
-      a: "Commas",
-      b: "Curly brackets",
-      c: "Quotes",
-      d: "Parentheses"
-    },
-    correctAnswer: "c"
-  },
-  {
-    question: "A very useful tool used during development and debugging for printing content to the debugger is:",
-    answers: {
-      a: "JavaScript",
-      b: "Terminal / Bash",
-      c: "For loops",
-      d: "Console.log"
-    },
-    correctAnswer: "d"
+
+
+var lastQuestion = questions.length - 1;
+var runningQuestion = 0;
+
+// ask a question
+function renderQuestion() {
+  var q = questions[runningQuestion];
+  question.innerHTML = "<p>" + q.question + "</p>";
+  choiceA.innerHTML = q.choiceA;
+  choiceB.innerHTML = q.choiceB;
+  choiceC.innerHTML = q.choiceC;
+  choiceD.innerHTML = q.choiceD;
+}
+
+
+// check answer
+// if answer is incorrect, display incorrect and subtract 10_000ms from timer
+// else, display correct and timer continues as is
+function checkAnswer(answer) {
+  if (answer !== questions[runningQuestion].correct) {
+    display = "incorrect";
+    timeLeft.value -= 10;
+  } else {
+    display = "correct!";
   }
-];
 
-// document.getElementById("startBtn").addEventListener("click", displayQuestion);
+  if (runningQuestion < lastQuestion) {
+    runningQuestion++;
+    renderQuestion();
+  } else {
+    clearInterval(timeInterval)
+    scoreRender();
+  }
+}
 
-// function displayQuestion() {
-//   var question = questions.pop();
-//   if (question) {
-//     var answer = confirm(question.q);
-//     if (answer === question.ans) {
-//       alert("Correct");
-//     }
-//     else {
-//       alert("Wrong");
-//     }
-//   }
-// };
+
+
+
+
 
 // //highscore
 // var initials = document.querySelector("initials").value;
@@ -112,6 +144,5 @@ var myQuestions = [
 
 
 
-startBtn.onclick = countdown;
-
+start.addEventListener("click", startQuiz);
 
